62c787e236e143734d4e9d790ffeb0f177143c6d
Jacob Williams <jacob@sevenfifty.com>
2023-03-25 13:49:47 -0400

    enforce uniquness constraint for student enrollments

--- app/models/student_section.rb
+++ app/models/student_section.rb
@@ -1,4 +1,5 @@
 class StudentSection < ApplicationRecord
   belongs_to :student
   belongs_to :section
+  validates :section_id, uniqueness: { scope: :student_id }
 end
--- test/models/student_section_test.rb
+++ test/models/student_section_test.rb
@@ -15,4 +15,17 @@ class StudentSectionTest < ActiveSupport::TestCase
     student_section = StudentSection.new section: section, student: student
     assert student_section.save
   end
+
+  test 'validate uniqueness constraint between a student and a section' do
+    teacher = Teacher.create first_name: 'Brian', last_name: 'Nguyen'
+    subject = Subject.create name: 'Geography'
+    teacher_subject = TeacherSubject.create teacher: teacher, subject: subject
+    section = Section.create teacher_subject: teacher_subject
+    student = Student.create first_name: 'Brian', last_name: 'Nguyen'
+
+    student_section = StudentSection.new section: section, student: student
+    student_section.save
+    invalid_student_section = StudentSection.new section: section, student: student
+    assert_not invalid_student_section.valid?
+  end
 end
